761cdb147e956094a9a343020ad4a0681b707cfc
erchiggins <emilyrchiggins@gmail.com>
2019-08-20 11:24:26 -0400

    adding import for CORS filter

--- src/main/java/com/revature/screenforce/ScreenforceGatewayServiceApplication.java
+++ src/main/java/com/revature/screenforce/ScreenforceGatewayServiceApplication.java
@@ -2,9 +2,10 @@ package com.revature.screenforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CORSFilter.class)
 public class ScreenforceGatewayServiceApplication {
 
 	public static void main(String[] args) {
